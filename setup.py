7480aa0e9cfabed7d7814f1e2f3c8a4850dd6149
Kosuke Akizuki <kosuke19952000@gmail.com>
2023-12-17 21:30:38 +0900

    Update

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as fh:
+    install_requires = fh.read().splitlines()
+
+setup(
+    name='Akkis Time-Series-Library',
+    version='0.1.0',
+    author='Akki',
+    author_email='me@akki.jp',
+    packages=find_packages(),
+    scripts=[],
+    url='https://github.com/akkijp/Time-Series-Library',
+    license='LICENSE',
+    description='An awesome time series library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+)
