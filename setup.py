6d71583754d232d7c3daa192095bbdac1d52933e
Kosuke Akizuki <kosuke19952000@gmail.com>
2023-12-18 23:47:23 +0900

    Upgrade

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as fh:
 
 setup(
     name='tslib',
-    version='0.1.0',
+    version='0.1.1',
     author='Akki',
     author_email='me@akki.jp',
     packages=find_packages(),
